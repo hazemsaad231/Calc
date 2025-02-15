bdaea02a9c2355cd95f51e742c6ef49cfe74e1b1
hazemsaad231 <166450789+hazemsaad231@users.noreply.github.com>
2025-02-15 18:41:38 +0200

    first commit

---
+++ calc.js
@@ -0,0 +1,25 @@
+let screen = document.getElementById('screen')
+
+let ahmed = document.getElementById('ahmed')
+
+
+function appendToScreen(input){
+    screen.value += input
+
+}
+
+
+function clearScreen(){
+    screen.value = ""
+    
+}
+function calcScreen(){
+    screen.value = eval(screen.value)
+
+}
+
+const func = ()=>{
+
+ahmed.onclick
+
+}
